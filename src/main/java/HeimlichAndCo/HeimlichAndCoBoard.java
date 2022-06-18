68ab2ee51f04130f5fa98102c9b0621e6ae12826
canzinger <e11904646@student.tuwien.ac.at>
2022-06-18 08:30:00 +0200

    more work on board

--- src/main/java/HeimlichAndCo/HeimlichAndCoBoard.java
+++ src/main/java/HeimlichAndCo/HeimlichAndCoBoard.java
@@ -4,27 +4,64 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class HeimlichAndCoBoard {
-    // save where the agents are
-    // save where the score markers of each agents are
+
+    /**
+     * saves which agents are in play
+     */
+    private final Agent[] agents;
 
     /**
      * saves the positions of each agent
      */
-    private Map<Agent, Integer> positions;
+    private Map<Agent, Integer> agentsPositions;
 
     /**
      * saves the current points of each agent
      */
     private Map<Agent, Integer> scores;
-
     private int safePosition;
+    private final static int numberOfFields = 12;
 
     // constructors
 
     public HeimlichAndCoBoard() {
-        this.positions = new HashMap<Agent, Integer>();
-        this.scores = new HashMap<Agent, Integer>();
+        agents = getParticipatingAgents(7); //default number of agents
+        this.agentsPositions = getAgentMapWithZeros(agents);
+        this.scores = getAgentMapWithZeros(agents);
         this.safePosition = 7; //the default starting position for the safe
+    }
+
+    public HeimlichAndCoBoard(int numberOfAgents) {
+        agents = getParticipatingAgents(numberOfAgents);
+        this.agentsPositions = getAgentMapWithZeros(agents);
+        this.scores = getAgentMapWithZeros(agents);
+        this.safePosition = 7; //the default starting position for the safe
+    }
+
+    /**
+     * moves an agent forward a certain number of fields
+     * @param a Agent to be moved
+     * @param numberOfFields number of fields the agent should be moved forward
+     */
+    public void moveAgent(Agent a, int numberOfFields) {
+        int oldPosition = agentsPositions.get(a);
+        agentsPositions.replace(a, (oldPosition + numberOfFields) % numberOfFields);
+    }
+
+    public void moveSafe(int fieldId) {
+        safePosition = fieldId;
+    }
+
+    /**
+     * awards all playings agents points according to their position on the board
+     */
+    public void awardPoints() {
+        for (Agent a : this.agents) {
+            int fieldId = agentsPositions.get(a);
+            int awardedPoints = getPointsForField(fieldId);
+            int oldPoints = scores.get(a);
+            scores.replace(a, awardedPoints + oldPoints);
+        }
     }
 
 
@@ -45,12 +82,40 @@ public class HeimlichAndCoBoard {
         }
     }
 
+    /**
+     *
+     * @param number number of participating agents
+     * @return Agent array with the participating agents
+     */
+    private Agent[] getParticipatingAgents(int number) {
+        Agent[] totalAgents = Agent.values();
+        Agent[] participatingAgents = new Agent[number];
+        for (int i = 0; i < number; i++) {
+            participatingAgents[i] = totalAgents[i];
+        }
+        return participatingAgents;
+    }
+
+    /**
+     *
+     * @param agents array of participating agents
+     * @return Map with an entry of 0 for each agent
+     */
+    private Map<Agent, Integer> getAgentMapWithZeros(Agent[] agents) {
+        Map<Agent, Integer> map = new HashMap<Agent, Integer>();
+        for (Agent a: agents) {
+            map.put(a, 0);
+        }
+        return map;
+    }
+
     @Override
     public HeimlichAndCoBoard clone() {
         HeimlichAndCoBoard newBoard = new HeimlichAndCoBoard();
         newBoard.safePosition = this.safePosition;
-        newBoard.positions = new HashMap<Agent, Integer>(this.positions); //TODO check if this is safe
-        newBoard.scores = new HashMap<Agent, Integer>(this.scores); //TODO check if this is safe
+        newBoard.agentsPositions = new HashMap<Agent, Integer>(this.agentsPositions); //TODO check if this is safe 100%
+        newBoard.scores = new HashMap<Agent, Integer>(this.scores); //TODO check if this is safe 100%
+        System.arraycopy(this.agents, 0, newBoard.agents, 0, this.agents.length);
         return newBoard;
         //TODO adapt to new variables in HeimlichAndCoBoard
     }
